d226c363a2f40752a951b897aa23042b1afc7e1d
Akbar <shyar.akbar@gmail.com>
2025-11-19 23:18:22 +0500

    server change

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
-const { createServer } = require('http');
-const next = require('next');
+import { createServer } from "http";
+import { parse } from "url";
+import next from "next";
 
-const app = next({ dev: false });
+const port = parseInt(process.env.PORT || "3000", 10);
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   createServer((req, res) => {
-    handle(req, res)
-  }).listen(process.env.PORT || 3000, () => {
-    console.log("Next.js app running...");
-  });
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  }).listen(port);
+
+  console.log(
+    `> Server listening at http://localhost:${port} as ${
+      dev ? "development" : process.env.NODE_ENV
+    }`
+  );
 });
